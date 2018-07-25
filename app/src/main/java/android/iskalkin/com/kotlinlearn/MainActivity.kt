8fe0a20568b8d55814e5d79cb799064d8be451cf
Ilya Skalkin <s500il@ya.ru>
2018-07-25 23:37:46 +0300

    Initial commit

---
+++ app/src/main/java/android/iskalkin/com/kotlinlearn/MainActivity.kt
@@ -0,0 +1,12 @@
+package android.iskalkin.com.kotlinlearn
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main)
+    }
+}
